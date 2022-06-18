b5416b40b3ca903727f91b0f22f6b45bc95e3df6
Chris Ramón <rchristian.ramon@gmail.com>
2022-06-18 02:37:54 -0500

    island-perimeter: adds Go solution

--- island-perimeter/solution.go
+++ island-perimeter/solution.go
@@ -1,68 +1,63 @@
-// Complexity Analysis
-// Time: O(M*N) where M and N are the rows & columns from the grid.
-// Space: O(M*N) where M x N is the size of the callstack during the dfs depth.
+// **Complexity Analysis**
+// - Time Complexity: O(N) where n is each cell within the grid.
+// - Space Complexity: O(N) where n is the queue size during the breadth first search.
 
 func islandPerimeter(grid [][]int) int {
-    // 7 * 4 - (6 * 2) = 28 - 12 = 16
-    // n * 4 - (n-1 * 2)
-    // 4 * 4 - (3 * 2) = 16 - 6 = 10
-    // 16 - (4-1 * 2) = 16 - 6 = 10
-    if(len(grid) == 0) {
-        return 0
+    result := 0
+    q := [][]int{}
+    for i := 0; i < len(grid); i++ {
+        if i == 1 {
+            break
+        }
+        for j := 0; j < len(grid[i]); j++ {
+            q = append(q, []int{i, j})
+        }
     }
-    
-    var count int
-    dfs(grid, 0, 0, &count)
-    if count == 0 {
-        return 0
+    for len(q) > 0 {
+        temp := [][]int{}
+        for len(q) > 0 {
+            n := q[0]
+            q = q[1:]
+            x, y := n[0], n[1]
+            count := countPerimeter(grid, x, y)
+            result += count
+            next := nextCell(grid, x, y)
+            if len(next) > 0 {
+                temp = append(temp, next)
+            }
+        }
+        q = temp
     }
-    // fmt.Printf("grid: %+v\n", grid)
-    return (count * 4) - ((count - 1) * 2)
+    return result
 }
 
-func dfs(grid [][]int, i int, j int, count *int) {
-    // fmt.Printf("i: %v\n", i)
-    // fmt.Printf("j: %v\n", j)
-    // fmt.Printf("grid: %v\n", grid)
-    if i < 0 ||j < 0 {
-        return
+func countPerimeter(grid [][]int, x int, y int) int {
+    if grid[x][y] == 0 {
+        return 0
     }
-    if i > len(grid) - 1 || j > len(grid[i]) - 1 {
-        return
-    } 
-    
-    if grid[i][j] == -1 || (grid[i][j] == 0 && (i > 0 || j > 0) && *count > 0) {
-        return
+    result := 0
+    // up
+    if (x-1 >= 0 && grid[x-1][y] == 0) || x-1 < 0 {
+        result += 1
     }
-    
-    if grid[i][j] == 1 {
-        *count += 1
+    // down
+    if (x+1 <= len(grid)-1 && grid[x+1][y] == 0) || x+1 > len(grid)-1 {
+        result += 1
     }
-    
-    grid[i][j] = -1
-    
-    dfs(grid, i - 1, j, count)
-    dfs(grid, i, j + 1, count)
-    dfs(grid, i + 1, j, count)
-    dfs(grid, i, j - 1, count)
+    // left
+    if (y-1 >= 0 && grid[x][y-1] == 0) || y-1 < 0 {
+        result += 1
+    }
+    // right
+    if (y+1 <= len(grid[x])-1 && grid[x][y+1] == 0) || y+1 > len(grid[x])-1 {
+        result += 1
+    }
+    return result
 }
 
-// Testcases:
-/*
-[[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
-[[0,1,0,1],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
-[[0,1,0,1],[1,1,1,0],[0,1,0,0],[1,1,0,3]]
-[[0,1,0,1],[1,1,1,0],[0,1,0,0],[1,1,0,-1]]
-[]
-[[0]]
-[[0],[1],[1]]
-[[0],[1],[1],[0,1]]
-[[0],[1],[1],[0]]
-[[-1],[-2]]
-[[],[]]
-[[0,0,1]]
-[[0,0,0,0],[0,0,0,0],[0,0,0,1]]
-[[1,0,0,0],[0,0,0,0],[0,0,0,1]]
-[[0,0,0,0],[0,0,1,0],[0,0,0,0]]
-[[1,1],[1,1]]
-*/
+func nextCell(grid [][]int, x int, y int) []int {
+    if x >= len(grid)-1 {
+        return []int{}
+    }
+    return []int{x+1,y}
+}
