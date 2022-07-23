c07fa14648fa6d24f93244e03e207b6d95b892d5
Chris Ramón <rchristian.ramon@gmail.com>
2022-07-23 02:44:31 -0500

    adds ransom-note solution

---
+++ ransom-note/solution.go
@@ -0,0 +1,17 @@
+// **Complexity Analysis**
+// * Time: O(M + N), where M & N are the length of the given strings.
+// * Space: O(1), constant number of variables used.
+
+func canConstruct(ransomNote string, magazine string) bool {
+	letters := [27]int{}
+	for i := 0; i < len(magazine); i++ {
+		letters[magazine[i]%97]++
+	}
+	for i := 0; i < len(ransomNote); i++ {
+		if letters[ransomNote[i]%97] == 0 {
+			return false
+		}
+		letters[ransomNote[i]%97]--
+	}
+	return true
+}
