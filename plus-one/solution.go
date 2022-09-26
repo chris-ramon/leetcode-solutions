4eda15548b592a5b786a74e583b86961ec0e8b51
Chris Ramón <rchristian.ramon@gmail.com>
2022-09-25 22:04:37 -0500

    adds plus-one test cases

--- plus-one/solution.go
+++ plus-one/solution.go
@@ -15,3 +15,8 @@ func plusOne(digits []int) []int {
 	digits = append([]int{1}, digits...)
 	return digits
 }
+
+/*
+TestCases
+[1,2,3]\n[4,3,2,1]\n[9,9]\n[0]\n[7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6]\n
+*/
