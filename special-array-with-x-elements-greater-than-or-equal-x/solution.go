f76beea9567823cd5a30989b5e148c4a45d462f4
Chris Ramón <rchristian.ramon@gmail.com>
2024-06-27 12:26:50 -0500

    special-array-with-x-elements-greater-than-or-equal-x: adds solution

---
+++ special-array-with-x-elements-greater-than-or-equal-x/solution.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func specialArray(nums []int) int {
+	// size & > than N.
+	sort.Slice(nums, func(a, b int) bool {
+		return nums[a] < nums[b]
+	})
+	nums2 := []int{}
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums2 = append(nums2, nums[i])
+		}
+	}
+	size := len(nums2)
+	max := nums2[len(nums2)-1]
+	for i := 0; i < max; i++ {
+		if i <= nums2[0] && i == size {
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+	fmt.Println(specialArray([]int{3, 5}))
+}
